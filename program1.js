51052b26aab36c49457313afc91335c6787d0630
farman712 <0126cs211043@oriental.ac.in>
2024-09-30 14:44:36 +0530

    Auto commit on save

--- program1.js
+++ program1.js
@@ -10,7 +10,7 @@ var isValid = function(s) {
         ']': '['
     };
     for (let i = 0; i < s.length; i++) {
-        let char = expression[i];
+        let char = s[i];
         if (char === '(' || char === '{' || char === '[') {
             stack.push(char);
         }
